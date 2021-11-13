7134612c25b2130380010f796b9f109f3b8ce7d2
web-developer032 <mubasherali0331@gmail.com>
2021-11-14 00:06:30 +0500

    set stipe link

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ export const bookTour = async (tourId) => {
       "pk_test_51Jtad3Cwtn9e5snmkaRVv6iu3JIN8XWCLsmoeRsbS81kovOZ40wWUIzsI59EY3LPx7q8jWIlxN6g8RLPpz3QclPN009K3kkmfv"
     );
     // 1) GET CHECKOUT SESSION FROM API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // 2) CREATE CHECKOUT FORM AND CHANGE CREDIT CARD
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
